c85bc7901ceb29652924f05f31d6822fd702999d
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-11-20 13:35:43 -0500

    posting and logging in with jwt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    username: "",
+    password: "",
+    email: "",
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "accepts": "application/json",
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(this.state)
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        localStorage.setItem("token", response.jwt)
+        localStorage.setItem("user", response.user.username)
+        console.log(response, localStorage)});
+  };
+
+  componentDidMount(){
+    const token = localStorage.getItem("token")
+    if (token){
+      fetch("http://localhost:3000/profile", {
+        method: "GET",
+        headers: {Authorization: `Bearer ${token}`},
+      })
+      .then(response => response.json())
+      .then(console.log)
+    } else {
+      console.log("no")
+    }
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="Enter Username"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          value={this.state.password}
+          onChange={this.handleChange}
+        />
+        <input
+          type="text"
+          name="email"
+          placeholder="Enter Email"
+          value={this.state.email}
+          onChange={this.handleChange}
+        />
+        <input type="submit" value="Submit"></input>
+      </form>
+    );
+  }
 }
 
 export default App;
