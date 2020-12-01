77fa36ebf79318cc79f212c1cc6572622318de5e
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-01 16:40:28 -0500

    fetching pics proving difficult

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,11 +1,15 @@
 import React from "react";
+import {config} from "./config"
 
 export default class Gallery extends React.Component {
 
+  state = {
+    picturesArray: []
+  }
+
   getPics = () => {
-    fetch("https://api.instagram.com/v1/users/search")
-      .then((response) => response.json())
-      .then(response => console.log(response));
+    fetch(`https://www.instagram.com/lepganci/?__a=1`)
+      .then((response) => console.log(response))
   };
 
   render() {
