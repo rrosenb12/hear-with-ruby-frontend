6629b754b9b37329962729eb198fdc7e34b90118
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-01 01:56:28 -0500

    basic navbar styling for landscape

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,31 @@
-import React from "react"
-import {NavLink} from "react-router-dom"
+import React from "react";
+import { NavLink } from "react-router-dom";
 
-export default function NavBar(){
-    return(
-        <nav className="row">
-            <p className="col-2">Ruby's Story</p>
-            <p className="col-2">Team <br/>#HearWithRuby</p>
-            <p id="title" className="col-4">Hear With Ruby</p>
-            <p className="col-2">Help Hearing, <br/>With Ruby</p>
-            <p className="col-2">Gallery</p>
-        </nav>
-    )
+export default function NavBar() {
+  return (
+    <>
+      <nav id="body" className="row">
+        <p id="body" className="col-2">
+          Ruby's Story
+        </p>
+        <p id="body" className="col-2">
+          Team <br />
+          #HearWithRuby
+        </p>
+        <p id="title" className="col-4">
+          Hear With Ruby
+        </p>
+        <p id="body" className="col-2">
+          Help Hearing, <br />
+          With Ruby
+        </p>
+        <p id="body" className="col-2">
+          Gallery
+        </p>
+      </nav>
+        <div className="double-line" />
+        <div className="diamond" />
+        <div className="double-line__right" />
+    </>
+  );
 }
