b8aba4df628a1b80bb9712832ab959306fadc86f
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-11-30 22:57:12 -0500

    saved jwt to different branch

--- src/App.js
+++ src/App.js
@@ -2,75 +2,10 @@ import React from "react"
 import "./App.css";
 
 class App extends React.Component {
-  state = {
-    username: "",
-    password: "",
-    email: "",
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-    fetch("http://localhost:3000/users", {
-      method: "POST",
-      headers: {
-        "accepts": "application/json",
-        "content-type": "application/json"
-      },
-      body: JSON.stringify(this.state)
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        localStorage.setItem("token", response.jwt)
-        localStorage.setItem("user", response.user.username)
-        console.log(response, localStorage)});
-  };
-
-  componentDidMount(){
-    const token = localStorage.getItem("token")
-    if (token){
-      fetch("http://localhost:3000/profile", {
-        method: "GET",
-        headers: {Authorization: `Bearer ${token}`},
-      })
-      .then(response => response.json())
-      .then(console.log)
-    } else {
-      console.log("no")
-    }
-  }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input
-          type="text"
-          name="username"
-          placeholder="Enter Username"
-          value={this.state.username}
-          onChange={this.handleChange}
-        />
-        <input
-          type="password"
-          name="password"
-          placeholder="Enter Password"
-          value={this.state.password}
-          onChange={this.handleChange}
-        />
-        <input
-          type="text"
-          name="email"
-          placeholder="Enter Email"
-          value={this.state.email}
-          onChange={this.handleChange}
-        />
-        <input type="submit" value="Submit"></input>
-      </form>
+      <div>hi</div>
     );
   }
 }
