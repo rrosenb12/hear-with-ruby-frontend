539f26055c67869ddd69cadb0caf4b85e3f74d87
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2020-12-02 13:51:43 -0500

    was fetching but then application request limit reached

--- src/Gallery.js
+++ src/Gallery.js
@@ -1,23 +1,45 @@
 import React from "react";
-import {config} from "./config"
-
+import { config } from "./config";
 export default class Gallery extends React.Component {
+  rootUrl = "https://graph.instagram.com";
+  userId = "17841400784952251";
+  accessToken = config.lindsay;
 
   state = {
-    picturesArray: []
-  }
-
-  getPics = () => {
-    fetch(`https://www.instagram.com/lepganci/?__a=1`)
-      .then((response) => console.log(response))
+    media: [],
+    pics: [],
   };
 
+  componentDidMount() {
+    fetch(
+      `${this.rootUrl}/${this.userId}/media?access_token=${this.accessToken}&fields=id,timestamp`
+    )
+      .then((response) => response.json())
+      .then(console.log)
+      // .then((response) => {
+      //   this.setState({ media: response.data });
+      // });
+  }
+
+  // componentDidUpdate() {
+  //   this.state.media.map((media) => {
+  //     return fetch(
+  //       `${this.rootUrl}/${media.id}?access_token=${this.accessToken}&fields=media_url,caption`
+  //     )
+  //       .then((response) => response.json())
+  //       .then(
+  //         (response) =>
+  //           response.caption.includes("#HearWithRuby") &&
+  //           this.setState((previousState) => {
+  //             return { pics: [...previousState.pics, response] };
+  //           })
+  //       );
+  //   });
+  // }
+
   render() {
-    return (
-      <>
-        <h1>this will be the gallery</h1>
-        {this.getPics()}
-      </>
-    );
+    console.log("media:", this.state.media);
+    console.log("pics:", this.state.pics);
+    return <div></div>;
   }
 }
