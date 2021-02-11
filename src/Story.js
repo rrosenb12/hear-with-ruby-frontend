322d09ef99074473efbd7ac4e829bf72edf19369
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2021-02-11 16:59:33 -0500

    how do i interpolate react components

--- src/Story.js
+++ src/Story.js
@@ -1,7 +1,15 @@
-import React from "react"
+import React, {useState} from "react"
+import story1 from "./story1"
 
 export default function Story(){
+
+    const [slide, setSlide] = useState(1)
+
     return(
-        <p>story page</p>
+        <>
+        {slide === 1 ? null : <p onClick={() => setSlide(slide - 1)}>back</p>}
+        <p onClick={() => setSlide(slide + 1)}>next</p>
+        <story1.useState />
+        </>
     )
 }
---
+++ src/story1.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Story1(){
+    return(
+        <>
+            story 1
+        </>
+    )
+}
