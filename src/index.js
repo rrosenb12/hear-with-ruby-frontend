cda7d4c7c69e630649bed14c2cbf7c17cd1f606a
rrosenb12 <rebecca.ap.rosenberg@gmail.com>
2021-02-28 12:17:19 -0500

    getting rid of config

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-      <App myKey={myKey} lindsayKey={lindsayKey}/>
+      <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
